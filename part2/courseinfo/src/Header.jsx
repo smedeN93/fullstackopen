fe4dbc52d0e01918423157b083d20dc07adf7ed8
smedeN93 <144838393+smedeN93@users.noreply.github.com>
2025-05-11 19:33:13 +0200

    Delete part2/courseinfo directory

--- part2/courseinfo/src/Header.jsx
+++
@@ -1,7 +0,0 @@
-const Header = (props) => {
-    return (
-        <h1>{props.name}</h1>
-    )
-}
-
-export default Header
--- part2/courseinfo/src/Total.jsx
+++
@@ -1,8 +0,0 @@
-const Total = (props) => {
-    const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
-return (
-  <p>Number of exercises {total}</p>
-);
-}
-
-export default Total
