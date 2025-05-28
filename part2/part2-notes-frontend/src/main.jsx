f2c04306444cbf7261db5acf6912a0648c6f91fc
Nicklas <nicklasmc93@gmail.com>
2025-05-28 16:08:24 +0200

    macbook switch

---
+++ part2/part2-notes-frontend/src/main.jsx
@@ -0,0 +1,34 @@
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import axios from 'axios'
+
+
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  console.log(notes)
+})
+
+const promise2 = axios.get('http://localhost:3001/foobar')
+console.log(promise2)
+
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true,
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    important: false,
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true,
+  },
+]
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App notes={notes} />
+)
