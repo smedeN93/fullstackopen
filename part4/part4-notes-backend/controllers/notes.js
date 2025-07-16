c78f0cf5ffbaf32558526bb8ca42bbe7aa8f7f74
Nicklas <nicklasmc93@gmail.com>
2025-07-16 16:29:14 +0200

    part4 user admin

--- part4/part4-notes-backend/controllers/notes.js
+++ part4/part4-notes-backend/controllers/notes.js
@@ -1,5 +1,6 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({})
@@ -21,13 +22,20 @@ notesRouter.get('/:id', async (request, response, next) => {
 
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
+  const user = await User.findById(body.userId)
+  if (!user) {
+    return response.status(400).json({ error: 'user not found' })
+  } 
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-  })
+    user: user._id,
+    })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
   response.status(201).json(savedNote)
 })
 
